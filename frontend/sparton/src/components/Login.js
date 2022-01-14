815deafe1b1761627219d6d0d58780acb4f6cea6
Ambrose <ambrosehundal0082@gmail.com>
2022-01-13 22:37:25 -0800

    frontend app created with login page

---
+++ frontend/sparton/src/components/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+const Login = () => {
+    return (
+        <div>
+            <form>
+                <label>
+                    Login
+                </label>
+                <input type="text"></input>
+            </form>
+        </div>
+    )
+}
+
+
+export default Login;
