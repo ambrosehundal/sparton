b97e530a3e2cf76cbd816ce39c68c208f9ddf96e
Ambrose <ambrosehundal0082@gmail.com>
2022-02-01 23:50:29 -0800

    new routes added

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,6 @@ const mongoose = require('mongoose');
 
 const mongoDbUri = 'mongodb://localhost/react-authentication';
 
-
-const app = express();
-
 mongoose.connect(mongoDbUri, function(err){
     if(err){
         throw err;
@@ -16,6 +13,21 @@ mongoose.connect(mongoDbUri, function(err){
 });
 
 
+const app = express();
+
+
+app.get('/api/home', (req, res) => {
+    res.send('Welcome!');
+});
+
+app.get('/api/courses', function(req, res) {
+    res.send('Come learn from the best mma site in the world');
+});
+  
+app.get('/', (req, res) => {
+    res.send('Best MMA online course in the world!')
+});
+
 app.listen(3000, () => {
     console.log(`Example app listening on port ${3000}`)
-  })
+});
