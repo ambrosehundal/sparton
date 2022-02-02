dd343b477e9ef78e330fef3c1fa86486d65a75d4
Ambrose <ambrosehundal0082@gmail.com>
2022-02-02 15:33:22 -0800

    home component added

--- frontend/sparton/src/App.js
+++ frontend/sparton/src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Login from './components/Login';
 import Register from './components/Register';
+import Home from './components/Home';
 import {Link, Route, Switch} from 'react-router-dom';
 
 
 const App = () => {
   return (
     <div className="App">
+      <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/register">Register</Link></li>
+      </ul>
+
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/login" exact component={Login} />
+
+    </Switch>
+
     <Login/>
   </div>
 
---
+++ frontend/sparton/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Home = () => {
+    return(
+        <div>
+            <p>Welcome to the best place to learn and develop MMA skills</p>
+        </div>
+    )
+}
+
+
+export default Home;
