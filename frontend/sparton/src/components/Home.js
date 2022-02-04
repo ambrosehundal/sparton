b815eedd137898b8db40356b047dad74ba3acf2f
Ambrose <ambrosehundal0082@gmail.com>
2022-02-03 23:51:34 -0800

    fixed fetch api to retrieve response from backend :)

--- frontend/sparton/src/components/Home.js
+++ frontend/sparton/src/components/Home.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 const Home = () => {
+
+    const [message, setMessage] = useState("Yo");
+
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/home')
+        .then(response => response.json())
+        .then(json => setMessage(json));
+    }, []);
     return(
         <div>
             <p>Welcome to the best place to learn and develop MMA skills</p>
+            <p>{message}</p>
         </div>
     )
 }
